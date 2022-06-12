2fa97380bb747ad045d64a7101928cbcaebbf739
Guillaume Patin <guillaume.patin@gmail.com>
2022-06-12 13:37:52 +0200

    feat: isolated layout and i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,53 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ProductSheetElements from "./views/ProductPage/ProductSheet";
+import { Divider, Layout, Menu } from "antd";
+import { LayoutOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
+
+import "./i18n";
+import ProductSheet from "./views/ProductPage/ProductSheet";
 
 const rootElement = document.getElementById("root");
 // to prevent TS compilation error
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
+
+const { Header, Footer, Sider, Content } = Layout;
+
+const PageScraperLayout = ({ children }: { children: React.ReactNode }) => {
+  const { t } = useTranslation("layout");
+  return (
+    <Layout>
+      <Header>
+        Le header présentera les actions possibles sur cette page (url à
+        scraper...)
+      </Header>
+      <Layout>
+        <Sider theme="light">
+          <Menu>
+            <Menu.Item>
+              <LayoutOutlined />
+              <span className="menu-item-title">{t("menu_product_sheet")}</span>
+              <Divider></Divider>
+              <span className="menu-item-desc">
+                {t("menu_product_sheet_desc")}
+              </span>
+            </Menu.Item>
+            <Menu.Item>
+              <LayoutOutlined />
+              <span className="menu-item-title">{t("menu_category_page")}</span>
+              <span className="menu-item-desc">
+                {t("menu_category_page_desc")}
+              </span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Content>{children}</Content>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  );
+};
 
 /**
  * the default route is /applications
@@ -13,6 +55,8 @@ const root = createRoot(rootElement);
  */
 root.render(
   <StrictMode>
-    <ProductSheetElements />
+    <PageScraperLayout>
+      <ProductSheet />
+    </PageScraperLayout>
   </StrictMode>
 );
--- src/views/ProductPage/ProductSheet.tsx
+++ src/views/ProductPage/ProductSheet.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import {
-  Layout,
-  Row,
-  Col,
-  Breadcrumb,
-  PageHeader,
-  Rate,
-  Empty,
-  List,
-} from "antd";
+import { Row, Col, Breadcrumb, PageHeader, Rate, Empty, List } from "antd";
 import {
   HomeOutlined,
   FolderOpenOutlined,
@@ -17,106 +8,91 @@ import {
 import { FiPackage } from "react-icons/fi";
 import "../../style.css";
 
-const { Header, Footer, Sider, Content } = Layout;
-
-const ProductSheetElements: React.FC = () => {
+const ProductSheet: React.FC = () => {
   return (
-    <Layout>
-      <Header>
-        Le header présentera les actions possibles sur cette page (url à
-        scraper...)
-      </Header>
-      <Layout>
-        <Sider theme="light">
-          Le sider présenter ici les différents layouts possibles (Product
-          sheet, category page)
-        </Sider>
-        <Content>
+    <>
+      <Row>
+        <Col>
+          <Breadcrumb>
+            <Breadcrumb.Item href="">
+              <HomeOutlined />
+            </Breadcrumb.Item>
+            <Breadcrumb.Item href="">
+              <FolderOpenOutlined />
+              <span>Category</span>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item href="">
+              <FolderOpenOutlined />
+              <span>Sub Category</span>
+            </Breadcrumb.Item>
+          </Breadcrumb>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <PageHeader title="Product Title" subTitle="This is a subtitle" />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Rate allowHalf defaultValue={2.5} /> Ratings
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <Row>
             <Col>
-              <Breadcrumb>
-                <Breadcrumb.Item href="">
-                  <HomeOutlined />
-                </Breadcrumb.Item>
-                <Breadcrumb.Item href="">
-                  <FolderOpenOutlined />
-                  <span>Category</span>
-                </Breadcrumb.Item>
-                <Breadcrumb.Item href="">
-                  <FolderOpenOutlined />
-                  <span>Sub Category</span>
-                </Breadcrumb.Item>
-              </Breadcrumb>
+              <Empty
+                image="assets/media-gallery.png"
+                imageStyle={{
+                  height: 240,
+                }}
+              ></Empty>
             </Col>
-          </Row>
-          <Row>
-            <Col>
-              <PageHeader title="Product Title" subTitle="This is a subtitle" />
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Rate allowHalf defaultValue={2.5} /> Ratings
-            </Col>
-          </Row>
-          <Row>
             <Col>
               <Row>
                 <Col>
-                  <Empty
-                    image="assets/media-gallery.png"
-                    imageStyle={{
-                      height: 240,
-                    }}
-                  ></Empty>
+                  <List
+                    size="large"
+                    header={
+                      <div>
+                        <DollarOutlined />
+                        Prices
+                      </div>
+                    }
+                    bordered
+                    dataSource={[
+                      "Discount Price",
+                      "Recommeded Price",
+                      "Unit",
+                      "currency",
+                    ]}
+                    renderItem={(item) => <List.Item>{item}</List.Item>}
+                  />
                 </Col>
+              </Row>
+              <Row>
                 <Col>
-                  <Row>
-                    <Col>
-                      <List
-                        size="large"
-                        header={
-                          <div>
-                            <DollarOutlined />
-                            Prices
-                          </div>
-                        }
-                        bordered
-                        dataSource={[
-                          "Discount Price",
-                          "Recommeded Price",
-                          "Unit",
-                          "currency",
-                        ]}
-                        renderItem={(item) => <List.Item>{item}</List.Item>}
-                      />
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col>
-                      <List
-                        size="large"
-                        header={
-                          <div>
-                            <FiPackage />
-                            Delivery info
-                          </div>
-                        }
-                        bordered
-                        dataSource={["Mode", "Delay", "Price", "currency"]}
-                        renderItem={(item) => <List.Item>{item}</List.Item>}
-                      />
-                    </Col>
-                  </Row>
+                  <List
+                    size="large"
+                    header={
+                      <div>
+                        <FiPackage />
+                        Delivery info
+                      </div>
+                    }
+                    bordered
+                    dataSource={["Mode", "Delay", "Price", "currency"]}
+                    renderItem={(item) => <List.Item>{item}</List.Item>}
+                  />
                 </Col>
               </Row>
             </Col>
           </Row>
-        </Content>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
+        </Col>
+      </Row>
+    </>
   );
 };
 
-export default ProductSheetElements;
+export default ProductSheet;
