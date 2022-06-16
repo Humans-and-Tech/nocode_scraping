5e8c32a6931ba3e9c9fdcc94bb87c0f1b177fac3
Guillaume Patin <guillaume.patin@gmail.com>
2022-06-16 16:42:54 +0200

    feat: 🎸 onboarding logic working

--- frontend/src/interfaces/index.ts
+++ frontend/src/interfaces/index.ts
@@ -1,3 +1,16 @@
+export enum PageType {
+  ProductSheet = 'ProductSheet',
+  CategoryPage = 'CategoryPage'
+}
+
+export interface WebsiteConfig {
+  url: string;
+  // proxy is usually a complex JSON object in scrapy
+  // very dependant from the proxy provider
+  // let's stringify it by simplicity
+  proxy: string;
+}
+
 export interface ScrapingElement {
   name: string | undefined;
   label?: string;
--- frontend/src/views/OnBoarding/OnBoarding.tsx
+++ frontend/src/views/OnBoarding/OnBoarding.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import {
     Space,
     Steps,
@@ -9,7 +9,7 @@ import {
     Input
 } from "antd";
 
-
+import { PageType, WebsiteConfig } from '../../interfaces'
 import { useTranslation } from "react-i18next";
 
 import { SocketContext } from "../../socket";
@@ -20,12 +20,52 @@ const { Step } = Steps;
 const { Meta } = Card;
 const { TextArea } = Input;
 
+
+
 const OnBoarding: React.FC = () => {
     const { t } = useTranslation("onboarding");
 
     const socket = useContext(SocketContext);
 
     const [currentStep, setCurrentStep] = useState<number>(0);
+
+    const [pageType, setPageType] = useState<PageType | undefined>(undefined);
+
+    const [websiteConfig, setWebsiteConfig] = useState<WebsiteConfig | undefined>(undefined);
+
+    const reset = () => {
+        // TODO
+        // reset the localStorage
+        setCurrentStep(0);
+    };
+
+    const nextStep = () => {
+        if (currentStep < 2) {
+            setCurrentStep(currentStep + 1);
+        }
+    };
+
+    const previousStep = () => {
+        if (currentStep > 0) {
+            setCurrentStep(currentStep - 1);
+        }
+    };
+
+    const chooseProductSheet = () => {
+        setPageType(PageType.ProductSheet);
+    };
+
+    const chooseCategoryPage = () => {
+        setPageType(PageType.CategoryPage);
+    };
+
+
+    useEffect(() => {
+        // store this config
+        // in the localStorage
+        // let's define a WebsiteConfigProvider
+        // that we'll pass in the context of the next pages
+    }, [pageType, websiteConfig]);
 
     return (
         <>
@@ -36,39 +76,66 @@ const OnBoarding: React.FC = () => {
                     <Step title={t('step.finished')} description={t('step.finished_desc')} />
                 </Steps>
 
-                <Input placeholder={t('configure.website_placeholder')} />
-                <TextArea placeholder={t('configure.proxy_placeholder')} />
+                {currentStep == 0 &&
+                    <>
+                        <Input placeholder={t('configure.website_placeholder')} />
+                        <TextArea placeholder={t('configure.proxy_placeholder')} />
 
-                <Space split={<Divider type="vertical" style={{ 'height': '300px' }} />}>
-                    <Card
-                        hoverable
-                        style={{ width: 240, 'margin': '1em 2em' }}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                    >
-                        <Meta title={t('page_type.product_sheet')} description={t('page_type.product_sheet_desc')} />
-                    </Card>
+                        <Button type="primary" onClick={nextStep}>
+                            {t('action.next_step')}
+                        </Button>
+                    </>
+                }
 
-                    <Card
-                        hoverable
-                        style={{ width: 240, 'margin': '1em 2em' }}
-                        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-                    >
-                        <Meta title={t('page_type.category_page')} description={t('page_type.category_page_desc')} />
-                    </Card>
+                {currentStep == 1 &&
+                    <>
+                        <Space split={<Divider type="vertical" style={{ 'height': '300px' }} />}>
+                            <Card
+                                hoverable
+                                style={{ width: 240, 'margin': '1em 2em' }}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                                onClick={chooseProductSheet}
+                            >
+                                <Meta title={t('page_type.product_sheet')} description={t('page_type.product_sheet_desc')} />
+                            </Card>
 
-                </Space>
+                            <Card
+                                hoverable
+                                style={{ width: 240, 'margin': '1em 2em' }}
+                                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                                onClick={chooseCategoryPage}
+                            >
+                                <Meta title={t('page_type.category_page')} description={t('page_type.category_page_desc')} />
+                            </Card>
+                            <Space>
+                                <Button type="primary" onClick={previousStep}>
+                                    {t('action.previous_step')}
+                                </Button>
+                                <Button type="primary" onClick={nextStep}>
+                                    {t('action.next_step')}
+                                </Button>
+                            </Space>
+                        </Space>
 
-                <Result
-                    status="success"
-                    title={t('finished.start_scraping')}
-                    subTitle={t('finished.start_scraping_subtitle')}
-                    extra={[
-                        <Button type="primary" key="console">
-                            {t('finished.start_scraping_action')}
-                        </Button>,
-                    ]}
-                />
+                    </>
+                }
 
+
+                {currentStep == 2 &&
+                    <Result
+                        status="success"
+                        title={t('finished.start_scraping')}
+                        subTitle={t('finished.start_scraping_subtitle')}
+                        extra={[
+                            <Button type="primary" key="start_scraping">
+                                {t('action.start_scraping_action')}
+                            </Button>,
+                            <Button key="reset" onClick={reset}>
+                                {t('action.reset')}
+                            </Button>,
+                        ]}
+                    />
+                }
             </Space>
 
         </>
