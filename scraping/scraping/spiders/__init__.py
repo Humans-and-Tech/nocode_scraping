f7e8d6dfadb93e8d18311d415e7e42d346fc8057
Guillaume Patin <guillaume.patin@gmail.com>
2022-08-07 17:50:14 +0200

    feat: 🎸 working on scrapy spider

--- scraping/scraping/spiders/__init__.py
+++ scraping/scraping/spiders/__init__.py
@@ -49,8 +49,5 @@ class FirestoreSpider(scrapy.Spider):
             yield next_url
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        with open(filename, "wb") as f:
-            f.write(response.body)
-        self.log(f"Saved file {filename}")
+        print(f"do some stuff with {response}")
+        pass
